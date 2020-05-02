f32cbd83f491bbd42e64be360312847a2fa0a816
Ankit.P <ankit.p@ezdi.us>
2020-05-02 19:23:32 +0530

    UI Changes

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -5,14 +5,27 @@ import { AppComponent } from './app.component';
 import { NgxFancyLoggerModule, LogLevel } from 'ngx-fancy-logger';
 import { ExampleComponent } from './example/example.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { CodeSnippetComponent } from './code-snippet/code-snippet.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'demo', component: ExampleComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    HomeComponent,
+    CodeSnippetComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     NgxFancyLoggerModule
   ],
   providers: [],
---
+++ projects/demo/src/app/code-snippet/code-snippet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-snippet',
+  templateUrl: './code-snippet.component.html',
+  styleUrls: ['./code-snippet.component.css']
+})
+export class CodeSnippetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
